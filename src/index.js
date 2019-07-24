776abbf84c149d4325e0e269454195bf7797dbeb
vreznik <v.reznik@weblium.com>
2019-07-25 00:53:26 +0300

    finish Animation steps

--- src/index.js
+++ src/index.js
@@ -1,48 +1,108 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import src from "./Group.png";
-import "./styles.css";
+import websiteSrc from "./images/website.png";
+import website2Src from "./images/website2.jpg";
+import "./styles.scss";
 import { useState } from "react";
+import classNames from "classnames"
+import SvgComponent from './svg'
 
 function App() {
-  const [Data, setAnimaton] = useState({
-    active: false,
-    overlay: false,
-    slide: false
+  const [StepsAnimation, setAnimaton] = useState({
+    scale: false,
+    hideImage: false,
+    slideSecond: false,
+    changeText: false,
+    showDomain: false,
   });
 
   const scaleToggle = () => {
-    setAnimaton({ active: !Data.active });
+    setAnimaton({...StepsAnimation, scale: !StepsAnimation.scale });
+  };
+
+  const hideImage = () => {
+    setAnimaton({...StepsAnimation, hideImage: !StepsAnimation.hideImage});
+  };
+
+  const showSecondImage = () => {
+    setAnimaton({...StepsAnimation, showSecondImage: !StepsAnimation.showSecondImage});
   };
 
   const changeSlide = () => {
-    setAnimaton({ active: !Data.active });
-    setTimeout(() => {
-      setAnimaton({ slide: !Data.active });
-    }, 1000);
+    setAnimaton({...StepsAnimation,  slideSecond: !StepsAnimation.slideSecond});
   };
 
-  const clear = () => {
+  const showDomainMask = () => {
+    setAnimaton({...StepsAnimation,  showDomain: !StepsAnimation. showDomain});
+  };
+
+  const changeDomainText = () => {
+    setAnimaton({...StepsAnimation,  changeText: !StepsAnimation. changeText});
+  };
+
+  const clearState = () => {
     setAnimaton({
-      active: false,
-      overlay: false,
-      slide: false
+      scale: false,
+      slideSecond: false,
+      changeText: false,
+      showDomain: false,
     });
   };
 
   return (
-    <div className="App">
-      <aside className="aside">
-
-        <button className="btn" onClick={scaleToggle}>
-        Scale
-        </button>
-      </aside>
-      <div className="wrapper">
-        <div className="image-container">
-          <img src={src} className={Data.active ? "img img-active" : "img"} />
+    <div className="wizard">
+      <aside className="wizard__aside">
+        <div className="wizard__form">
+          <button className="btn btn-abs btn-mb" onClick={clearState}> CLEAR STATE</button>
+          <button className="btn btn-mb" onClick={scaleToggle}>Scale image</button>
+          <button className="btn btn-mb" onClick={hideImage}>Hide image</button>
+          <button className="btn btn-mb" onClick={changeSlide}>Change slide</button>
+          <button className="btn btn-mb" onClick={showDomainMask}>Show Domain Mask</button>
+          <button className="btn" onClick={changeDomainText}>Change Domain text</button>
         </div>
-      </div>
+      </aside>
+      <main className="wizard__main">
+        <div className="slider">
+          <div className={
+            classNames(
+              "slider__slides",
+              {[`slider__slides--hide`]: StepsAnimation.hideImage},
+            )
+          }>
+            <div
+              className={
+                classNames(
+                  "slider__item",
+                  {[`slider__item--active`]: !StepsAnimation.slideSecond},
+                  {[`slider__item--scale`]: StepsAnimation.scale},
+                  {[`slider__item--translate`]: StepsAnimation.hideImage}
+                  )
+              }>
+              <SvgComponent />
+              <img src={websiteSrc} className="image" />
+              <div className="slider__text-domain">
+                {StepsAnimation.changeText ? 'https://gooooooooooogle.com' : 'https://app.weblium.com'}
+              </div>
+            </div>
+            <div
+              className={
+                classNames(
+                  "slider__item",
+                  {[`slider__item--active`]: StepsAnimation.slideSecond},
+                  {[`slider__item--scale`]: StepsAnimation.scale},
+                  {[`slider__item--translate`]: StepsAnimation.hideImage},
+                  )
+              }>
+              <SvgComponent/>
+              <img src={website2Src} className="image" />
+              <div className="slider__text-domain">
+                {StepsAnimation.changeText ? 'https://gooooooooooogle.com' : 'https://app.weblium.com'}
+              </div>
+            </div>
+          </div>
+          <div className={classNames("slider__domain", {[`slider__domain--show`]: StepsAnimation.showDomain && StepsAnimation.scale})}/>
+        </div>
+      </main>
     </div>
   );
 }
