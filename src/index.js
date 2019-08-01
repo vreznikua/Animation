3d127ca04f37e1312f3f8e75862112a01b2fd50f
vreznikweblium <v.reznik@weblium.com>
2019-08-01 11:53:15 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -8,137 +8,35 @@ import classNames from "classnames"
 import SvgComponent from './svg'
 
 function App() {
-  const [StepsAnimation, setAnimaton] = useState({
-    scale: false,
-    hideImage: false,
-    slideSecond: false,
-    prepareSecond: false,
-    showSecondImage: false,
-    changeText: false,
-    showDomain: false,
-  });
-
-  const scaleToggle = () => {
-    setAnimaton({...StepsAnimation, scale: !StepsAnimation.scale });
-  };
-
-  const hideImage = () => {
-    setAnimaton({...StepsAnimation, hideImage: !StepsAnimation.hideImage});
-  };
-
-  const prepareSecond = () => {
-    setAnimaton({...StepsAnimation, prepareSecond: !StepsAnimation.prepareSecond});
-  };
-
-  const showSecondImage = () => {
-    setAnimaton({...StepsAnimation, showSecondImage: !StepsAnimation.showSecondImage});
-  };
-
-  const changeSlide = () => {
-    setAnimaton({...StepsAnimation,  slideSecond: !StepsAnimation.slideSecond});
-  };
-
-  const showDomainMask = () => {
-    setAnimaton({...StepsAnimation,  showDomain: !StepsAnimation. showDomain});
-  };
-
-  const changeDomainText = () => {
-    setAnimaton({...StepsAnimation,  changeText: !StepsAnimation. changeText});
-  };
-
-  const endChageSlidesScene = () => {
-    setAnimaton({...StepsAnimation, hideImage: false, prepareSecond:false,showSecondImage:false });
-  };
-
-
-  const clearState = () => {
-    setAnimaton({
-      scale: false,
-      hideImage: false,
-      slideSecond: false,
-      prepareSecond: false,
-      showSecondImage: false,
-      changeText: false,
-      showDomain: false,
-    });
-  };
-
-  const startSlideShow = () => {
-    hideImage()
-    setTimeout(function() {
-      changeSlide()    
-  }, 525); 
-    setTimeout(function() {
-      prepareSecond() 
-  }, 500); 
-
-
+  
+  const createCanvasWindow = () => {
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d');
   }
+
+  createCanvasWindow()
 
   return (
     <div className="wizard">
       <aside className="wizard__aside">
         <div className="wizard__form">
-          <button className="btn btn-abs" onClick={clearState}> CLEAR STATE</button>
-          <button className="btn btn-abs btn-abs-l" onClick={startSlideShow}>Start animation</button>
+          <button className="btn btn-abs"> CLEAR STATE</button>
+          <button className="btn btn-abs btn-abs-l">Start animation</button>
           <div className="btn-mb">
-            <button className="btn" onClick={hideImage}>Hide image</button>
-            <button className="btn" onClick={prepareSecond}>Prepare</button>
-            <button className="btn" onClick={showSecondImage}>Show Second</button>
-            <button className="btn" onClick={endChageSlidesScene}>End</button>
+            <button className="btn">Hide image</button>
+            <button className="btn">Prepare</button>
+            <button className="btn">Show Second</button>
+            <button className="btn">End</button>
           </div>
-          <button className="btn btn-mb" onClick={changeSlide}>Change slide</button>
-          <button className="btn btn-mb" onClick={scaleToggle}>Scale image</button>
-          <button className="btn btn-mb" onClick={showDomainMask}>Show Domain Mask</button>
-          <button className="btn" onClick={changeDomainText}>Change Domain text</button>
+          <button className="btn btn-mb">Change slide</button>
+          <button className="btn btn-mb">Scale image</button>
+          <button className="btn btn-mb">Show Domain Mask</button>
+          <button className="btn">Change Domain text</button>
         </div>
       </aside>
       <main className="wizard__main">
         <div className="slider">
-          <div className={
-            classNames(
-              "slider__slides",
-              {[`slider__slides--hide`]: StepsAnimation.hideImage},
-              {[`slider__slides--prepare`]: StepsAnimation.prepareSecond},
-              {[`slider__slides--show-second`]: StepsAnimation.showSecondImage},
-            )
-          }>
-            <div
-              className={
-                classNames(
-                  "slider__item",
-                  {[`slider__item--active`]: !StepsAnimation.slideSecond},
-                  {[`slider__item--scale`]: StepsAnimation.scale},
-                  {[`slider__item--translate`]: StepsAnimation.hideImage},
-                  {[`slider__item--prepare`]: StepsAnimation.prepareSecond},
-                  {[`slider__item--show-second`]: StepsAnimation.showSecondImage},
-                  )
-              }>
-              <SvgComponent />
-              <img src={websiteSrc} className="image" />
-              <div className="slider__text-domain">
-                {StepsAnimation.changeText ? 'https://gooooooooooogle.com' : 'https://app.weblium.com'}
-              </div>
-            </div>
-            <div
-              className={
-                classNames(
-                  "slider__item",
-                  {[`slider__item--active`]: StepsAnimation.slideSecond},
-                  {[`slider__item--scale`]: StepsAnimation.scale},
-                  {[`slider__item--translate`]: StepsAnimation.hideImage},
-                  {[`slider__item--prepare`]: StepsAnimation.prepareSecond},
-                  {[`slider__item--show-second`]: StepsAnimation.showSecondImage},
-                  )
-              }>
-              <SvgComponent/>
-              <img src={website2Src} className="image" />
-              <div className="slider__text-domain">
-                {StepsAnimation.changeText ? 'https://gooooooooooogle.com' : 'https://app.weblium.com'}
-              </div>
-            </div>
-          </div>
-          <div className={classNames("slider__domain", {[`slider__domain--show`]: StepsAnimation.showDomain && StepsAnimation.scale})}/>
+          <div id="canvas"/>
         </div>
       </main>
     </div>
